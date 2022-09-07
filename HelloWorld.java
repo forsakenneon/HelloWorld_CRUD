8fdc689e9e2f4b2b08fd3b486d5290fe1c4d0878
I938L <89921118+I938L@users.noreply.github.com>
2022-09-07 19:15:13 +0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,44 @@
+package com.srccodes.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public HelloWorld() {
+        super();
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println("<h1>Hello World! GET</h1>");
+    }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	 response.setContentType("text/html");
+         PrintWriter printWriter = response.getWriter();
+         printWriter.println("<h1>Hello World! POST</h1>");
+    }
+    
+    
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	 response.setContentType("text/html");
+         PrintWriter printWriter = response.getWriter();
+         printWriter.println("<h1>Hello World! PUT</h1>");
+    }
+    
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	 response.setContentType("text/html");
+         PrintWriter printWriter = response.getWriter();
+         printWriter.println("<h1>Hello World! DELETE</h1>");
+    }
+}
