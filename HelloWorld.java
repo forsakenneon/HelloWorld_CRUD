bef12cc1dc239c43353cf2ddc6e1e93fc535fb6e
I938L <89921118+I938L@users.noreply.github.com>
2022-09-07 19:22:40 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ package com.srccodes.example;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,7 +27,6 @@ public class HelloWorld extends HttpServlet {
          PrintWriter printWriter = response.getWriter();
          printWriter.println("<h1>Hello World! POST</h1>");
     }
-    
     
     protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	 response.setContentType("text/html");
